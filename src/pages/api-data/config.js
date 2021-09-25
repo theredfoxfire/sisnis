f1efc3953bf5ee553f83c087b1dad2cfca4e7737
vizzlearn@gmail.com <vizzlearn@gmail.com>
2021-09-25 17:23:31 +0700

    set build config

--- src/pages/api-data/config.js
+++ src/pages/api-data/config.js
@@ -9,7 +9,7 @@ import { ensureObject } from "../../utils/objectUtils";
 let { auth, userDetail } = getAllState();
 
 export const axiosWorker = axios.create({
-  baseURL: `103.150.113.100:3200/public/${getRoleEndpoint()}`,
+  baseURL: `http://103.150.113.100:3200/public/${getRoleEndpoint()}`,
   timeout: 10000,
   headers: { Authorization: `Bearer ${auth.token}` },
 });
